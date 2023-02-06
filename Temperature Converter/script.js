e4edfd0355e76c1bafd8f093bdba260f0a90199a
Hema Hariharan Samson <86085398+HemahariharanSamson@users.noreply.github.com>
2023-02-07 01:55:06 +0530

    Add files via upload

---
+++ Temperature Converter/script.js
@@ -0,0 +1,26 @@
+const inputs = document.querySelectorAll("input");
+const c= document.querySelector("#celsius");
+const f= document.querySelector("#fahrenheit");
+const k= document.querySelector("#kelvin");
+
+inputs.forEach(input =>{
+    input.addEventListener("input",e => {
+        const unit =e.target.id;
+        const v= parseInt(e.target.value);
+        if(unit === "celsius"){
+            f.value=parseFloat((v*1.8)+32).toFixed(4)*1;
+            k.value=parseFloat(v+274.15).toFixed(4)*1;
+        }
+        else if(unit==="fahrenheit"){
+            c.value = parseFloat((v-32)*5/9).toFixed(4)*1;
+            k.value= parseFloat(((v-32)*5/9)+273.15).toFixed(4)*1;
+        }
+        else if(unit ==="kelvin"){
+            c.value = parseFloat(v-273.15).toFixed(4)*1;
+            f.value = parseFloat((v-273.15)*9/5+32).toFixed(4)*1;
+        }
+
+    });
+});
+
+document.addEventListener('contextmenu', event => event.preventDefault());
